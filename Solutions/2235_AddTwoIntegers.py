2ed26005b8a4e3b32749d5e72778c4102f7980c7
Mahesh Kakde <mahesh.kakde.165@gmail.com>
2026-09-01 19:34:22 +0530

    add 2235-add two int

---
+++ Solutions/2235_AddTwoIntegers.py
@@ -0,0 +1,10 @@
+# case 1
+case1_1, case1_2 = 12, 5
+# case 2
+case2_1, case2_2 = -10, 4
+
+def sol(num1, num2):
+    return num1 + num2
+
+print(sol(case1_1, case1_2))
+print(sol(case2_1, case2_2))
