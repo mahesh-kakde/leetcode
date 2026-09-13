65ea85225d13c4ce7c782e6457fe66a90368c52e
Mahesh Kakde <mahesh.kakde.165@gmail.com>
2026-09-13 21:03:15 +0530

    add 121-best time to buy sell stock

---
+++ Solutions/121_BestTimeToBuyAndSellStock.py
@@ -0,0 +1,19 @@
+prices1 = [7,1,5,3,6,4]
+prices2 = [7,6,4,3,1]
+
+def sol(prices):
+    min_price = prices[0]
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        profit = price - min_price
+
+        if profit > max_profit:
+            max_profit = profit
+
+    return max_profit
+
+print(sol(prices1))
+print(sol(prices2))
